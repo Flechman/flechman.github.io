f07768a3635464c5340100f82f1377016ee8c939
Remi <remi.delacourt@gmail.com>
2023-02-14 17:43:48 +0100

    Modify config file

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'Rémi Delacourt',
+    description: 'A website to host all my notes and thoughts',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Notes', link: '/notes' }
+      ]
+    }
+  }
