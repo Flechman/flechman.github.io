7cab1c6d19ccfbc23416a4263957af0b0aaf685b
Remi <remi.delacourt@gmail.com>
2023-12-23 00:38:03 +0100

    Fix prismjs script

--- scripts/integrate_prism.py
+++ scripts/integrate_prism.py
@@ -2,10 +2,10 @@ import json
 import shutil
 
 # Copy the prism-da-pseudocode.js inside the "components" folder of the prism package
-shutil.copyfile("../../docs/.vuepress/components/prism/prism-da-pseudocode.js", "../../node_modules/prismjs/components/prism-da-pseudocode.js")
+shutil.copyfile("../docs/.vuepress/components/prism/prism-da-pseudocode.js", "../node_modules/prismjs/components/prism-da-pseudocode.js")
 
 # Edit components.json of the prism package, add the following to the "languages" object:
-with open('../../node_modules/prismjs/components.json', 'r+') as f:
+with open('../node_modules/prismjs/components.json', 'r+') as f:
     components = json.load(f)
     if "da-pseudocode" not in components["languages"]:
         components["languages"]["da-pseudocode"] = {"title": "Distributed Algo Pseudocode", "owner": "Flechman"}
