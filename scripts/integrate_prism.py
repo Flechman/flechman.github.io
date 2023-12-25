0edab96e786dec46fa33b078e6fc2dc87bcc1136
Delacourt <remi.delacourt@gmail.com>
2023-12-25 16:04:53 +0100

    Fix path problem in script

--- scripts/integrate_prism.py
+++ scripts/integrate_prism.py
@@ -3,15 +3,13 @@ import shutil
 import os
 
 # Copy the prism-da-pseudocode.js inside the "components" folder of the prism package
-print(os.getcwd())
-print(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
-shutil.copyfile("../docs/.vuepress/components/prism/prism-da-pseudocode.js", "../node_modules/prismjs/components/prism-da-pseudocode.js")
+shutil.copyfile(os.getcwd()+"/docs/.vuepress/components/prism/prism-da-pseudocode.js", os.getcwd()+"/node_modules/prismjs/components/prism-da-pseudocode.js")
 
 # Edit components.json of the prism package, add the following to the "languages" object:
-with open('../node_modules/prismjs/components.json', 'r+') as f:
+with open(os.getcwd()+"/node_modules/prismjs/components.json", 'r+') as f:
     components = json.load(f)
     if "da-pseudocode" not in components["languages"]:
         components["languages"]["da-pseudocode"] = {"title": "Distributed Algo Pseudocode", "owner": "Flechman"}
     f.seek(0) # reset file position to the beginning.
     json.dump(components, f, indent=4)
-    f.truncate() # remove remaining part
+    f.truncate() # remove remaining parts
