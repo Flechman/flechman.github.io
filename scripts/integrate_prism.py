258a3b9079a0886cafc8ada6bb8926c267ed4fc4
Delacourt <remi.delacourt@gmail.com>
2023-12-25 17:33:07 +0100

    Test print

--- scripts/integrate_prism.py
+++ scripts/integrate_prism.py
@@ -15,3 +15,4 @@ with open(os.getcwd()+"/node_modules/prismjs/components.json", 'r+') as f:
     f.seek(0) # reset file position to the beginning.
     json.dump(components, f, sort_keys=True, indent=4)
     f.truncate() # remove remaining parts
+    print(f.read())
