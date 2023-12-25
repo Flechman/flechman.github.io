30ac0e0090177f6d7c54a863e201588537c3364f
Delacourt <remi.delacourt@gmail.com>
2023-12-25 16:18:10 +0100

    Add printing in the script

--- scripts/integrate_prism.py
+++ scripts/integrate_prism.py
@@ -3,9 +3,11 @@ import shutil
 import os
 
 # Copy the prism-da-pseudocode.js inside the "components" folder of the prism package
+print("-> Copying "+os.getcwd()+"/docs/.vuepress/components/prism/prism-da-pseudocode.js"+" into "+os.getcwd()+"/node_modules/prismjs/components")
 shutil.copyfile(os.getcwd()+"/docs/.vuepress/components/prism/prism-da-pseudocode.js", os.getcwd()+"/node_modules/prismjs/components/prism-da-pseudocode.js")
 
 # Edit components.json of the prism package, add the following to the "languages" object:
+print("-> Edit "+os.getcwd()+"/node_modules/prismjs/components.json"+" to add language definition 'da-pseudocode' inside the 'languages' object")
 with open(os.getcwd()+"/node_modules/prismjs/components.json", 'r+') as f:
     components = json.load(f)
     if "da-pseudocode" not in components["languages"]:
