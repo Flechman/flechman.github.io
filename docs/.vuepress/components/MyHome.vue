dfa869fa9c7b3e548eef8f45972fcaed1996366a
Rémi Delacourt <54138269+Flechman@users.noreply.github.com>
2025-09-21 23:50:57 +0200

    Update homepage

--- docs/.vuepress/components/MyHome.vue
+++ docs/.vuepress/components/MyHome.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="presentation">
         <p style="text-align: center;">
-            Hi! My name is Rémi Delacourt, I'm a Research Engineer at Mistral AI in Paris, France. At the moment, my main focus is on Systems for Machine Learning, more specifically LLM Serving Systems. I aim to serve LLMs faster and with fewer resources. This is an exciting topic, as the algorithmic and systems sides are tied together!
+            Hi! My name is Rémi Delacourt, I'm a Research Engineer at Mistral AI in Paris, France. At the moment, my main focus is on Large-scale Machine Learning Systems. I help design the best models in the world given hardware- and system-level constraints, and then maximize their speed and efficiency. This is an exciting topic, as the algorithmic and systems sides are tied together!
             If you want to discuss, I'd be happy to exchange on current, new, or upcoming technologies and their impact on the world with you! You can shoot me an email at <a href = "mailto: remi.delacourt@gmail.com">remi.delacourt@gmail.com</a> anytime.
         </p>
     </div>
